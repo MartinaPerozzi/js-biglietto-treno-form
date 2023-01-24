f56fc14d4cf6516b6e54cf13605e2fb8546bce0d
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-24 16:45:31 +0100

    html + js- in progress

--- js/myscript.js
+++ js/myscript.js
@@ -1,46 +1,5 @@
 // Calcolare il prezzo totale del viaggio in treno
 
-// Dati necessari: numero di km da percorrere e età del passeggero
-
-// Chiedere all'utente il numero di km che deve percorrere
-const myKm = parseInt(prompt("Numero di Km da percorrere:"));
-console.log("myKm", myKm);
-
-// Chiedere all'utente la sua età
-const myAge = parseInt(prompt("Età passeggero:"));
-console.log("myAge", myAge);
-
-
-
-// CALCOLARE PREZZO
-
-// Prezzo al km
-let xKm = parseFloat(0.21);
-console.log("xKm", xKm);
-
-// calcolo
-let kmPercorso = (myKm * xKm);
-console.log("kmPercorso=", `(${myKm} * ${xKm})`, kmPercorso);
-let fixedNumber = kmPercorso.toFixed(2);
-
-
-// Applicare SCONTI
-
-// Se l'utente è minorenne Age<18
-if (myAge < 18) {
-    let kmPercorso = (myKm * xKm) - (((myKm * xKm) * 20) / 100);
-    console.log(kmPercorso, "con sconto del 20%");
-
-    let fixedNumber = kmPercorso.toFixed(2);
-
-} else if (myAge >= 65) { //Se l'età dell'utente è >=65
-    let kmPercorso = (myKm * xKm) - (((myKm * xKm) * 40) / 100);
-    console.log(kmPercorso, "con sconto del 40%");
-
-    let fixedNumber = kmPercorso.toFixed(2);
-
-}
-
 // MILESTONE 1
 const button_submit_el = document.querySelector(".button-proceed");
 console.log(button_submit_el);
@@ -48,7 +7,35 @@ console.log(button_submit_el);
 button_submit_el.addEventListener(
     "click",
     function () {
-        if (myAge < 18) {
+
+        // Dati necessari: numero di km da percorrere e età del passeggero
+
+        // Chiedere all'utente il suo nome e cognome
+        const myName = document.querySelector("#name-surname").value;
+        console.log("myName", myName);
+
+        // Chiedere all'utente il numero di km che deve percorrere
+        const myKm = document.querySelector("input[type = 'number']").value;
+        console.log("myKm", myKm);
+
+        // Chiedere all'utente la sua età
+        const myAge = document.querySelector('#My-Age>option').value;
+        console.log("myAge", myAge);
+
+        // CALCOLARE PREZZO
+
+        // Prezzo al km
+        let xKm = parseFloat(0.21);
+        console.log("xKm", xKm);
+
+        // calcolo
+        let kmPercorso = (myKm * xKm);
+        console.log("kmPercorso=", `(${myKm} * ${xKm})`, kmPercorso);
+        let fixedNumber = kmPercorso.toFixed(2);
+
+        // APPLICARE SCONTI
+
+        if (myAge = "minorenne") {
             let kmPercorso = (myKm * xKm) - (((myKm * xKm) * 20) / 100);
             console.log(kmPercorso, "con sconto del 20%");
 
